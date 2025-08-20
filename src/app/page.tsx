4a8003dd19c86bc66e7a5f7812ae522586b61530
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2025-08-20 03:36:53 +0000

    feat: page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ThemeToggler } from "@/components/Theme/theme-toggler";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -27,45 +28,22 @@ export default function Home() {
           </li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button>Click me</Button>
+        <div className="flex gap-3 items-center flex-col sm:flex-row">
+          <Link
+            className=""
+            href="https://github.com/MuhammadKaifNazeer/boilerplate-nextjs-15-with-tailwindcss-v4-and-shadcn-ui"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button className="cursor-pointer">Github</Button>
+          </Link>
           <ThemeToggler />
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://muhammadkaifnazeer.vercel.app"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -76,8 +54,8 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
-        </a>
+          Created by Muhammad Kaif Nazeer →
+        </Link>
       </footer>
     </div>
   );
